177a09cc91f83a6c440cf488b0c4f3e7277cc607
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-16 17:33:14 -0300

    refactor: created binary_tree

---
+++ binary_tree.py
@@ -0,0 +1,43 @@
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    root: Node
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, node: int):
+        self.root = self.__insert_recursive(self.root, node)
+
+    def __insert_recursive(self, current: Node, node: int):
+        if current is None:
+            return Node(node)
+        
+        if node < current.value:
+            current.left = self.__insert_recursive(current.left, node)
+        elif node > current.value:
+            current.right = self.__insert_recursive(current.right, node)
+
+        return current
+    
+    def show(self):
+        self.__show_recursive(self.root, 0)
+
+    def __show_recursive(self, current: Node, level: int):
+        
+        if current:
+            if level > 0:
+                print("    " * (level - 1) + f"└── {current.value}")
+            else:
+                print(current.value)
+
+            self.__show_recursive(current.left, level + 1)
+            self.__show_recursive(current.right, level + 1)
