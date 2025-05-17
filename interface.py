6a4fa85bab66de11ba1f8204d9151c2859882acf
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-16 23:13:47 -0300

    feat: added interface to insert node in binary tree

--- interface.py
+++ interface.py
@@ -6,9 +6,19 @@ class Interface:
         self.option = None
 
     def show_menu(self):
-        print("-----------ÁRVORE BINÁRIA-----------\n")
+        print("\n-----------ÁRVORE BINÁRIA-----------\n")
         print("1. Inserir valor")
         print("2. Mostrar árvore")
-        print("3. Sair\n")
+        print("3. Pesquisar valor")
+        print("4. Remover valor")
+        print("5. Sair")
 
-        self.option = input('Opção: ')
+
+    def choose_option(self):
+        self.option = int(input('\nOpção: '))
+
+    def get_value(self) -> int:
+        value = int(input('\nValor: '))
+
+        return value 
+
--- main.py
+++ main.py
@@ -6,30 +6,65 @@ if __name__ == '__main__':
     binary_tree = BinaryTree()
     interface = Interface()
 
-    binary_tree.insert(node=20)
+    while interface.option != 5:
+        interface.show_menu()
+        interface.choose_option()
 
-    binary_tree.insert(node=25)
-    binary_tree.insert(node=10)
+        match interface.option:
+            case 1:
+                node = interface.get_value()
 
-    binary_tree.insert(node=8)
-    binary_tree.insert(node=30)
-    binary_tree.insert(node=22)
+                binary_tree.insert(node=node)
 
-    binary_tree.insert(node=35)
-    binary_tree.insert(node=23)
+            case 2:
+                print('\n')
+                binary_tree.show()
 
-    binary_tree.insert(node=5)
-    binary_tree.insert(node=9)
+            case 3:
+                node = interface.get_value()
 
-    binary_tree.show()
+                result = binary_tree.search(node=node)
+
+                print('\nEncontrado!\n') if result else print('\nNão encontrado\n')
+
+            case 4:
+                node = interface.get_value()
+
+                binary_tree.remove(node=node)
+
+                print('\nValor removido!\n')
+
+            case 5:
+                break
+
+            case _:
+                pass
+
+
+    # binary_tree.insert(node=20)
+
+    # binary_tree.insert(node=25)
+    # binary_tree.insert(node=10)
+
+    # binary_tree.insert(node=8)
+    # binary_tree.insert(node=30)
+    # binary_tree.insert(node=22)
+
+    # binary_tree.insert(node=35)
+    # binary_tree.insert(node=23)
+
+    # binary_tree.insert(node=5)
+    # binary_tree.insert(node=9)
+
+    # binary_tree.show()
 
     # Should returns False
-    print(binary_tree.search(node=77))
+    # print(binary_tree.search(node=77))
 
     # Should returns True
-    print(binary_tree.search(node=8))
+    # print(binary_tree.search(node=8))
 
     # Remove node 30
-    binary_tree.remove(node=30)
+    # binary_tree.remove(node=30)
 
-    binary_tree.show()
+    # binary_tree.show()
